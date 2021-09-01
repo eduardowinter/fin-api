e9cd6438c5f5879db61114ce6677a01d25594111
Eduardo Winter <eduardo.wbess@outlook.commit>
2021-09-01 08:29:01 -0300

    added account update and account deletion functionalities

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const customers = [];
 function verifyIfExistsAccountCPF(request, response, next) {
   const { cpf } = request.headers;
 
-  const customer = customers.find(customer => customer.cpf = cpf);
+  const customer = customers.find((customer) => customer.cpf === cpf);
 
   if (!customer) {
     return response.status(400).json({ error: "Customer not found"});
@@ -112,5 +112,36 @@ app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
 
   return response.json(statement);
 })
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(200).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+  const {customer } = request;
+
+  customers.splice(customer, 1);
+
+  return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer. statement);
+
+  return response.json(balance);
+});
 
 app.listen(3333);
